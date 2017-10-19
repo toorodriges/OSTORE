215685b90dd16e865478f01533de297d54c9b895
toorodriges <toorodriges@gmail.com>
2017-10-19 22:26:39 +0300

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://rodriges.pp.ua/admin/');
+define('HTTP_CATALOG', 'http://rodriges.pp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://rodriges.pp.ua/admin/');
+define('HTTPS_CATALOG', 'http://rodriges.pp.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/skorvin1/data/www/rodriges.pp.ua/admin/');
+define('DIR_SYSTEM', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/');
+define('DIR_IMAGE', '/var/www/skorvin1/data/www/rodriges.pp.ua/image/');
+define('DIR_LANGUAGE', '/var/www/skorvin1/data/www/rodriges.pp.ua/admin/language/');
+define('DIR_TEMPLATE', '/var/www/skorvin1/data/www/rodriges.pp.ua/admin/view/template/');
+define('DIR_CONFIG', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/config/');
+define('DIR_CACHE', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/download/');
+define('DIR_LOGS', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/skorvin1/data/www/rodriges.pp.ua/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'skorvin1');
+define('DB_PASSWORD', 'LMJ5jEdhvT');
+define('DB_DATABASE', 'ostore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://rodriges.pp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://rodriges.pp.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/skorvin1/data/www/rodriges.pp.ua/catalog/');
+define('DIR_SYSTEM', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/');
+define('DIR_IMAGE', '/var/www/skorvin1/data/www/rodriges.pp.ua/image/');
+define('DIR_LANGUAGE', '/var/www/skorvin1/data/www/rodriges.pp.ua/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/skorvin1/data/www/rodriges.pp.ua/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/config/');
+define('DIR_CACHE', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/download/');
+define('DIR_LOGS', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/skorvin1/data/www/rodriges.pp.ua/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'skorvin1');
+define('DB_PASSWORD', 'LMJ5jEdhvT');
+define('DB_DATABASE', 'ostore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
